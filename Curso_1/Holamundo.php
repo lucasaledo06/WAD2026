6b48edea54d88d54a148d184aabe387a03de665a
Lucas Salas Ledo <152426317+lucasaledo06@users.noreply.github.com>
2026-05-14 20:39:48 +0200

    dos

--- Curso_1/Holamundo.php
+++ Curso_1/Holamundo.php
@@ -5,8 +5,19 @@
         <title>Prácticas php</title>
     </head>
     <body>
-        <?php
+        <?php 
+        # ======== TIPOS DE COMENTARIOS =========
+        // soy un comentario de una sola línea
+        /* Soy un comentario 
+        de líneas
+        múltiples
+        */
+        # Soy un comentario de consola Unix
             echo "hola mundo"
-        ?>
+        # ==== Mostrar por pantalla ====
+        echo( string $arg1 [, string $arg2]) : void # <-- Mostrar una o más cadenas (ligeramente más rápido)
+
+        print(string $arg) : int # <-- Mostrar una cadena (devuelve 1)        
+        ?>      
     </body>
 </html>
