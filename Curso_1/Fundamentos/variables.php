b1a86755064696e70d15567a5434318aa375468e
Lucas Salas Ledo <152426317+lucasaledo06@users.noreply.github.com>
2026-05-14 20:51:47 +0200

    avance1

---
+++ Curso_1/Fundamentos/variables.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Plantilla</title>
+    </head>
+    <body>
+        <?php 
+            //=== Definición de variables ===
+            $edad = 39; // <-- Variable de tipo integer (valor entero)
+            $estatura = 1.67; // <-- Variable de tipo float (decimales)
+            $nombre = "Luis Javier" //<-- Variable tipo string (texto)
+            $frase = "Luisja tiene $edad años" //<-- Concatenar la variable edad con el texto
+#            $frase = 'Luisja tiene $edad años' //<-- Con las comillas simples no podemos introducir variables en el texto
+            $profesor = true; //<-- Valor booleano (verdadero o falso)
+            /*Mostrar datos por pantalla*/
+
+        ?>      
+    </body>
+</html>
