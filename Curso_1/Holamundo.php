f6913740383014884c5b2f692aa071cd2ea5ac5a
Lucas Salas Ledo <152426317+lucasaledo06@users.noreply.github.com>
2026-05-14 20:32:40 +0200

    Introducción

---
+++ Curso_1/Holamundo.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Prácticas php</title>
+    </head>
+    <body>
+        <?php
+            echo "hola mundo"
+        ?>
+    </body>
+</html>
